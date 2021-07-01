7971cf5bc9bcc1a5c31accccd15472f815304f7b
MariaGuadalupeChan <15470140@campeche.tecnm.mx>
2021-06-30 23:48:12 -0500

    primer commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { StocksService, StockInterface } from './services/stocks.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'stocks';
+
+ // Declara una propiedad como un arreglo de acciones
+ stocks: Array<StockInterface> = [];
+ constructor(service: StocksService) {
+ service.load(['AAPL']).subscribe(stocks => {
+ // Una vez que se cargan los datos los almacena en la propiedad stocks.
+ this.stocks = stocks;
+ });
+ }
+}
